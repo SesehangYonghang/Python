ec18b49f621740a0d40faabec4145433cff7e835
SesehangYonghang <sesehangyonghang50@gmail.com>
2025-08-19 09:02:38 +0545

    Add files via upload

---
+++ SImple Form.py
@@ -0,0 +1,10 @@
+print("----- Simple User Form -----")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+email = input("Enter your email: ")
+
+print("\n--- Form Submitted ---")
+print("Name :", name)
+print("Age  :", age)
+print("Email:", email)
