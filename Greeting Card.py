f836c2abed4e2581707a9002d5ef03097dc7a883
SesehangYonghang <sesehangyonghang50@gmail.com>
2025-08-31 19:42:59 +0545

    Update Greeting Card.py

--- Greeting Card.py
+++ Greeting Card.py
@@ -1,12 +1,9 @@
 #Greeting Card Function
-def make_card(name = None, age = None, message = "Have a good day."):
-    if name is None:
-        name = input("Enter your Name: ")
-    
-    if age is None:
-        age = input("Enter your age: ")
-    
-    card = f"Hello {name}({age} years old)!\n{message}"
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+def make_card(name, age, message = "Have a good day."):
+    card = f"Hello {name}, ({age} years old!)\n{message}"
     return card
 
-print(make_card())
+print(make_card(name,age))
+
